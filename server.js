0de22a8d2348707f361622825253e6aa6ffcc11e
Brian Song <bsong070@gmail.com>
2021-01-04 13:59:52 -0500

    update error

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ let responseObject={}
 app.post('/api/shorturl/new',bodyParser.urlencoded({extended:false}), (request, response)=>{
   let inputUrl = request.body.url
 
-  let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
+  let urlRegex = new RegExp(/^[http://www.]/gi )
 
   if(!inputUrl.match(urlRegex)){
     response.json({ error: 'invalid url' })
