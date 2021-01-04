7198eb08528267738fe34946e6058f858902532d
Brian Song <bsong070@gmail.com>
2021-01-04 13:18:06 -0500

    complete

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const mongo = require('mongodb');
+
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useUnifiedTopology: true}) //for production
+
+// mongoose.connect("mongodb+srv://user1:0IcYwl3Qa3oJHdky@freecodecamp.4rcza.mongodb.net/db1?retryWrites=true&w=majority", 
+// {useNewUrlParser:true, 
+//   useUnifiedTopology: true}) //for locally
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,3 +31,62 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+let uri = process.env.MONGODB_URI
+
+let urlSchema = new mongoose.Schema({
+  original : {type: String, required: true},
+  short: Number
+})
+
+let Url = mongoose.model('Url',urlSchema)
+
+let bodyParser = require('body-parser')
+let responseObject={}
+app.post('/api/shorturl/new',bodyParser.urlencoded({extended:false}), (request, response)=>{
+  let inputUrl = request.body.url
+
+  let urlRegex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi)
+
+  if(!inputUrl.match(urlRegex)){
+    response.json({error: 'Invalid URL'})
+    return
+  }
+
+  responseObject['original_url'] = inputUrl
+
+  let inputShort = 1
+
+  Url.findOne({}) //will look through everything with no entry
+      .sort({short:'desc'}) //get back highest short number
+      .exec((error, result)=>{
+        if(!error && result !=undefined){ //first time running, will be no result entry
+          inputShort = result.short+1
+        }
+        if(!error){
+          Url.findOneAndUpdate(
+            {original:inputUrl}, //if theres one to replace
+            {original:inputUrl, short:inputShort}, //set for new or update
+            {new: true, upsert:true},
+            (error, savedUrl)=>{
+              if(!error){
+                responseObject['short_url'] = savedUrl.short
+                response.json(responseObject)
+              }
+            }
+          )
+        }
+      })
+})
+
+app.get('/api/shorturl/:input',(request,response)=>{
+  let input = request.params.input
+
+  Url.findOne({short: input}), (error,result)=>{
+    if(!error && result != undefined){
+      response.redirect(result.original)
+    }else{
+      response.json('URL Not Found')
+    }
+  }
+})
